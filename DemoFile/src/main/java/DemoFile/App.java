277cf56ff7745cb561fc53f1dff8000853868c59
e031950 <e031950@W00000PF4HZ26J.wg.dir.telstra.com>
2024-08-13 10:18:57 +0530

    firdt commit

---
+++ DemoFile/src/main/java/DemoFile/App.java
@@ -0,0 +1,13 @@
+package DemoFile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
